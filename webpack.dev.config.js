a48bdad442144aec1656f18015342807b80b9ac7
[3기]이원희 <ireba4478@gmail.com>
2020-10-30 15:28:35 +0900

    feat: webpack hot loader 설정

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,10 +1,19 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const {HotModuleReplacementPlugin} = require('webpack');
 
 module.exports = {
     mode: 'development',
     entry: './src/index.js',
-    plugins: [ new HtmlWebpackPlugin() ],
+    plugins: [ 
+        new HtmlWebpackPlugin(),
+        new HotModuleReplacementPlugin() 
+    ],
+    devServer: {
+        contentBase: path.resolve(__dirname, 'dist'),
+        hot: true,
+        open: true
+    },
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'dev.js'
