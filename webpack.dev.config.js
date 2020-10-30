1832f549dce46961ceff69f85bfc1c15354c8531
[3기]이원희 <ireba4478@gmail.com>
2020-10-30 15:15:08 +0900

    feat: html-webpack-plugin 설치

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,8 +1,10 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     mode: 'development',
     entry: './src/index.js',
+    plugins: [ new HtmlWebpackPlugin() ],
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'dev.js'
