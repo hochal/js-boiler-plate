7d1596127bc878ee175689913ca778000e825057
[3기]이원희 <ireba4478@gmail.com>
2020-11-02 02:20:46 +0900

    feat: css 관련 플러그인 설치

--- configs/webpack.base.config.js
+++ configs/webpack.base.config.js
@@ -1,5 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 
 module.exports = {
@@ -8,12 +9,6 @@ module.exports = {
     path: path.resolve(__dirname, '../dist'),
     filename: 'build.js'
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      hash: true // 불러오는 정적 빌드 파일에 쿼리스트링으로 해시값 추가
-    }),
-    new CleanWebpackPlugin()
-  ],
   module: {
     rules: [
       {
@@ -25,7 +20,23 @@ module.exports = {
             presets: ['@babel/preset-env']
           }
         }
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
       }
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      hash: true // 불러오는 정적 빌드 파일에 쿼리스트링으로 해시값 추가
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({filename: 'app.css'})
+  ]
 };
